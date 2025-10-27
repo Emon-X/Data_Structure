9db4be9f7c501a26e15ce983fcab001e20db55e2
Emon-X <mdemonhossaince23013@gmail.com>
2025-10-27 10:01:16 +0600

    Update

---
+++ DSU/Unionfind.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ll long long int
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define pp pop_back()
+#define mod 1000000007
+#define endl "\n"
+#define N 200005
+ll dx[] = {1, -1, 0, 0, 1, 1, -1, -1};
+ll dy[] = {0, 0, 1, -1, 1, -1, 1, -1};
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+struct DSU {
+    vector<ll> p, sz;
+    DSU(ll n) {
+        p.resize(n);
+        sz.assign(n, 1);
+        iota(all(p), 0);
+    }
+    ll find(ll x) {
+        return p[x] == x ? x : p[x] = find(p[x]);
+    }
+    void unite(ll a, ll b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            if (sz[a] < sz[b]) swap(a, b);
+            p[b] = a;
+            sz[a] += sz[b];
+        }
+    }
+    bool same(ll a, ll b) { return find(a) == find(b); }
+    ll size(ll a) { return sz[find(a)]; }
+};
+
+void domain_expension()
+{
+    ll n,q;
+    cin >> n >> q;
+    DSU st(n+1);
+    for(int i=1;i<=q;i++){
+
+        ll t,u,v;
+        cin >> t >> u >> v;
+        if(t==0) st.unite(u,v);
+        else{
+            if(st.same(u,v)) cout << 1 << endl;
+            else cout << 0 << endl;
+        }
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    //cin >> t;
+    while (t--)
+        domain_expension();
+    return 0;
+}
